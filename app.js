a3fb1b94e02580293e1377fe9a668b1fb6c93400
catherineblu <e.ropleva@gmail.com>
2020-12-12 16:28:01 +0200

    i rendered the json... yeey

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ class Products{
         try{
             let result = await fetch('products.json');
             let data = await result.json();
-            return data;
+
+            let products = data.items;
+            products = products.map(item => {
+                const {title,price} = item.fields;
+                const {id} = item.sys
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image}
+            })
+            return products
         }catch(error){
             console.log(error);
         }
@@ -27,7 +35,31 @@ class Products{
 }
 //displaying the items
 class UI{
-
+    displayProducts(products){
+        let result = '';
+        products.forEach(product => {
+            result +=`
+            <!--single product-->
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="Necklace-8"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to cart
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+        <!--end of single product-->
+            `;
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 //local storage
 class Storage{
@@ -37,5 +69,5 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const ui = new UI();
     const products = new Products();
     //get all products
-    products.getProducts().then(data => console.log(data));
+    products.getProducts().then(products => ui.displayProducts(products));
 });
