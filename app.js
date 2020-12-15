33c969f82f987c83d07282d81945ab85e42cc66f
catherineblu <e.ropleva@gmail.com>
2020-12-15 23:41:53 +0200

    successfully items showing in the console

--- app.js
+++ app.js
@@ -84,6 +84,8 @@ class UI {
         Storage.saveCart(cart)
         //set cart values
         this.setCartValues(cart);
+        //display cart item
+        this.addCartItem(cartItem);
         //add cart item
         //show the cart
       });
@@ -98,7 +100,24 @@ class UI {
       })
       cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
       cartItems.innerText = itemsTotal;
-      console.log(cartTotal,cartItems)
+  }
+  addCartItem(item){
+    const div = document.createElement('div');
+    div.classList.add('cart-item');
+    div.innerHTML = `<img src=${item.image} alt="Earring 2">
+    <div>
+        <h4>${item.title}</h4>
+        <h5>$${item.price}</h5>
+        <span class="remove-item" data-id=${item.id}
+        >remove</span>
+    </div>
+    <div>
+        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+        <p class="item-amount">${item.amount}</p>
+        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+    </div>`
+    cartContent.appendChild(div);
+    console.log(cartContent)
   }
 }
 //local storage
