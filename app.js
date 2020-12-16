9bf6a7dd662dae45db2c0a222a7f4a59cdf4706a
catherineblu <e.ropleva@gmail.com>
2020-12-16 11:45:07 +0200

    remove from cart functionality is builded

--- app.js
+++ app.js
@@ -138,11 +138,33 @@ class UI {
     cartOverlay.classList.remove('transparentBcg');
     cartDOM.classList.remove('showCart');
   }
+ 
   cartLogic(){
-    clearCartBtn.addEventListener('click',this.clearCart)
+     //clear cart button
+    clearCartBtn.addEventListener('click',() => {
+      this.clearCart();
+    });
+    //cart functionality
   }
   clearCart(){
-    console.log(this);
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => this.removeItem(id));
+    console.log(cartContent.children)
+    while(cartContent.children.length>0){
+      cartContent.removeChild(cartContent.children[0])
+    }
+    this.hideCart();
+  }
+  removeItem(id){
+    cart = cart.filter(item => item.id !==id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`
+  }
+  getSingleButton(id){
+    return buttonsDOM.find(button => button.dataset.id === id);
   }
 }
 //local storage
